f7c1e253dd28444770937731e632b484058c7ed9
nuwayyir1993 <nuwayyir.almohammadi@outlook.com>
2019-06-24 22:07:20 +0000

    My homework with bouns 1 :)

---
+++ main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    
+     
+        axios({
+            method:"get",
+            url:"https://api.imgflip.com/get_memes"
+                })
+
+ .then((info)=>{
+    console.log(info)
+    info.data.data.memes.forEach((response) => {
+                        $('h1').append(`<p>${response.name}</p>        
+        <img src=' ${response.url}' height='${response.height}' width='${response.width}'>`)
+                    })
+                    $('h1').on('click', function(event){
+                        $('p').html('')
+                        let breed = $(event.target).text()
+ })
+ 
+
+ })
+
+
+ .catch((error)=>{
+    $("p").text("request faild")        
+console.log("there was an error")
+})
+    })
