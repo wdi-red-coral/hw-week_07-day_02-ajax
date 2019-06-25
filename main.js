65940dd6c0ea40d8079370ceb258e1abfdab6827
Salman <alk-333@hotmail.com>
2019-06-25 07:30:57 +0300

    done

--- main.js
+++ main.js
@@ -6,15 +6,20 @@ $(document).ready(function () {
     .then(response => {
         response.data.data.memes.forEach( item => {
             $("body").append(`<div class="card" style="width: 18rem;">
-            <img src=${item.url} class="card-img-top">
+            <img src=${item.url} class="card-img-top" alt="${item.name}">
             <div class="card-body">
             <h5 class="card-title"><span class="badge badge-secondary">${item.name}</span></h5>
             </div>
             </div>
             `)
-            $("img").css({width: item.width, height: item.height})        
+            $("img").css({width: item.width, height: item.height, display: "none"})        
         }
         )
+        $(".card").click(function (e) { 
+            e.preventDefault();
+            $(this).find('img').toggle();
+            
+        });
     })
 
     .catch(error => $("body").append(`<h4>${error}</h4>`))
