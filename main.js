6acfdde19332589681a6d85b0e19af87c02e2a0d
akhouja1994 <a.khouja1994@gmail.com>
2019-06-25 09:18:22 +0300

    almost done

---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    axios({
+        method: 'get',
+        url: 'https://api.imgflip.com/get_memes'
+    })
+    .then(response => {
+        response.data.data.memes.forEach(meme => {
+            $('body').append(`<img class="${meme.name}" src="${meme.url}" height="${meme.height}" width="${meme.width}" alt="sorry" >`)
+            $('body').append(`<p>${meme.name}<p/>`)
+            
+        });
+        $('p').click(function(e){
+            $(`.${e.target}`).toggle();
+        })
+    })
+    .catch(error => {
+        $('p').text('')
+        console.log('')
+    })
+})
